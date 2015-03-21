b4128d0fb44ce1d0eba77e266004dd75bf334792
dougcampbell <ndouglascampbell@gmail.com>
2015-03-21 13:08:23 -0500

    added beginning class structure

--- CTApy.py
+++ CTApy.py
@@ -1,2 +1,17 @@
 __author__ = 'dougcampbell'
 
+import ConfigParser
+
+
+class CTApy:
+
+    def _init_(self):
+        #Read config file and get API key
+        config = ConfigParser.ConfigParser()
+        config.read("api.conf")
+        self.apikey = config.get("API", "apikey")
+
+
+
+
+
